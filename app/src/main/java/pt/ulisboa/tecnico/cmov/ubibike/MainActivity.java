2d9b358eb80f9abc3dc03f84a897684aacfca2f4
oliveira94 <arturoliveira@tecnico.ulisboa.pt>
2016-03-13 00:14:44 +0000

    So fiz alteraçoes em icons e cor do layout

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/MainActivity.java
@@ -7,16 +7,11 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
-
-
 
     public void SignInClicked(View view){
         Intent i = new Intent(this,LogIn.class);
