0d2de774d57c1204b24e6bd111d85cd013f80982
Nuno Vasconcelos <nunorogerio12@hotmail.com>
2016-03-14 17:43:47 +0000

    Chat implemented

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Chat.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/Chat.java
@@ -1,7 +1,17 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Chat extends AppCompatActivity {
 
@@ -9,5 +19,31 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+    }
+
+    public void sendClickedChat(View view) {
+        LinearLayout linearLayoutVertical = (LinearLayout) findViewById(R.id.idChatLinearVertical);
+        LinearLayout chatHorizontalLayout = new LinearLayout(this);
+
+        //Moving the text to the new text box
+        TextView chatText = new TextView(this);
+        EditText entryText = (EditText) findViewById(R.id.textEntryChat);
+        String text = entryText.getText().toString();
+        chatText.setText(text);
+        chatText.setTextSize(22);
+        chatText.setTextColor(Color.BLACK);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+
+        //Setting the parameters to the intended
+        chatHorizontalLayout.setGravity(Gravity.RIGHT);
+
+        //Adding the textView to the HorizontalLayout
+        chatHorizontalLayout.addView(chatText, params);
+
+        //Adding the whole HorizontalLayout to the VerticalLayout
+        linearLayoutVertical.addView(chatHorizontalLayout);
     }
 }
